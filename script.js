9e772b37076adb84152a6461c45949fbfb05cf19
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-29 01:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const fontSizeIp = document.getElementById("fontsize");
+const fontColorIp = document.getElementById("fontcolor");
+const SaveBtn = document.querySelector("input[type='submit']");
+// Load saved preferences from cookies
+if (document.cookie) {
+  const preferences = document.cookie.split(";").reduce((acc, cookie) => {
+    const [key, value] = cookie.split("=");
+    acc[key.trim()] = value.trim();
+    return acc;
+  }, {});
+
+  if (preferences.fontSize) {
+    fontSizeIp.value = preferences.fontSize;
+    document.documentElement.style.setProperty("--fontsize", preferences.fontSize + "px");
+  }
+
+  if (preferences.fontColor) {
+    fontColorIp.value = preferences.fontColor;
+    document.documentElement.style.setProperty("--fontcolor", preferences.fontColor);
+  }
+}
+
+// Save preferences to cookies on form submit
+SaveBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const fontSize = fontSizeIp.value;
+  const fontColor = fontColorIp.value;
+  document.documentElement.style.setProperty("--fontsize", fontSize + "px");
+  document.documentElement.style.setProperty("--fontcolor", fontColor);
+  document.cookie = `fontSize=${fontSize}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
+  document.cookie = `fontColor=${fontColor}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
+});
