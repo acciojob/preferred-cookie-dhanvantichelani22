f42f621cc1a90140ff8a82c74697b5250d658ba8
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-29 01:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
-const fontSizeIp = document.getElementById("fontsize");
-const fontColorIp = document.getElementById("fontcolor");
-const SaveBtn = document.querySelector("input[type='submit']");
+// Get form elements
+const fontSizeInput = document.getElementById("fontsize");
+const fontColorInput = document.getElementById("fontcolor");
+const saveButton = document.querySelector("input[type='submit']");
+
 // Load saved preferences from cookies
 if (document.cookie) {
   const preferences = document.cookie.split(";").reduce((acc, cookie) => {
@@ -11,21 +13,21 @@ if (document.cookie) {
   }, {});
 
   if (preferences.fontSize) {
-    fontSizeIp.value = preferences.fontSize;
+    fontSizeInput.value = preferences.fontSize;
     document.documentElement.style.setProperty("--fontsize", preferences.fontSize + "px");
   }
 
   if (preferences.fontColor) {
-    fontColorIp.value = preferences.fontColor;
+    fontColorInput.value = preferences.fontColor;
     document.documentElement.style.setProperty("--fontcolor", preferences.fontColor);
   }
 }
 
 // Save preferences to cookies on form submit
-SaveBtn.addEventListener("click", (event) => {
+saveButton.addEventListener("click", (event) => {
   event.preventDefault();
-  const fontSize = fontSizeIp.value;
-  const fontColor = fontColorIp.value;
+  const fontSize = fontSizeInput.value;
+  const fontColor = fontColorInput.value;
   document.documentElement.style.setProperty("--fontsize", fontSize + "px");
   document.documentElement.style.setProperty("--fontcolor", fontColor);
   document.cookie = `fontSize=${fontSize}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
