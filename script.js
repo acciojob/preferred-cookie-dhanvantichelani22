60750493d54a9efcf2c2297253b33c603cbf9d63
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-29 19:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,30 +6,32 @@ const saveButton = document.querySelector("input[type='submit']");
 
 // Load saved preferences from cookies
 if (document.cookie) {
-  const preferences = document.cookie.split(";").reduce((acc, cookie) => {
-    const [key, value] = cookie.split("=");
-    acc[key.trim()] = value.trim();
-    return acc;
-  }, {});
+const preferences = document.cookie
+.split(";")
+.reduce((acc, cookie) => {
+const [key, value] = cookie.trim().split("=");
+acc[key] = decodeURIComponent(value);
+return acc;
+}, {});
 
-  if (preferences.fontSize) {
-    fontSizeInput.value = preferences.fontSize;
-    document.documentElement.style.setProperty("--fontsize", preferences.fontSize + "px");
-  }
+if (preferences.fontSize) {
+fontSizeInput.value = preferences.fontSize;
+document.documentElement.style.setProperty("--fontsize", preferences.fontSize + "px");
+}
 
-  if (preferences.fontColor) {
-    fontColorInput.value = preferences.fontColor;
-    document.documentElement.style.setProperty("--fontcolor", preferences.fontColor);
-  }
+if (preferences.fontColor) {
+fontColorInput.value = preferences.fontColor;
+document.documentElement.style.setProperty("--fontcolor", preferences.fontColor);
+}
 }
 
 // Save preferences to cookies on form submit
 saveButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const fontSize = fontSizeInput.value;
-  const fontColor = fontColorInput.value;
-  document.documentElement.style.setProperty("--fontsize", fontSize + "px");
-  document.documentElement.style.setProperty("--fontcolor", fontColor);
-  document.cookie = `fontSize=${fontSize}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
-  document.cookie = `fontColor=${fontColor}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
+event.preventDefault();
+const fontSize = fontSizeInput.value;
+const fontColor = fontColorInput.value;
+document.documentElement.style.setProperty("--fontsize", fontSize + "px");
+document.documentElement.style.setProperty("--fontcolor", fontColor);
+document.cookie = `fontSize=${encodeURIComponent(fontSize)}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
+document.cookie = `fontColor=${encodeURIComponent(fontColor)}; expires=${new Date(Date.now() + 86400000).toUTCString()}`;
 });
